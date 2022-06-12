c216635f868af1be48df84387059c90201c2274e
guikoller <g_koller@hotmail.com>
2022-06-11 22:54:41 -0300

    ascii encode function criada

--- client.py
+++ client.py
@@ -2,11 +2,27 @@ from email import message
 from http import client
 import threading
 from socket import socket
+import tkinter as tk
+
+from numpy import *
+
+#window = tk.Tk()
+#window.title('CHAT')
+#window.geometry('400x300')
 
 name = input('Please Enter your Name : ')
 client = socket()
 port = int(input('Enter the port server Running on : '))
 client.connect(('localhost', port))
+
+
+#window.mainloop
+
+def asciiEncode(message):
+    values = []
+    for char in message:
+        values.append(ord(char))
+    return str(values)
 
 def receiveMessage():
     while True:
@@ -23,7 +39,7 @@ def receiveMessage():
 
 def sendMessage():
     while True:
-        message = '{} : {}'.format(name, input(''))
+        message = (asciiEncode('{} : {}'.format(name, input(''))))
         client.send(message.encode())
 
 threading.Thread(target=receiveMessage).start()
