9e31efbb9a24d867a77e3685494df6a45722b817
Mateus Marodim <marodim@alunos.utfpr.edu.br>
2022-06-29 02:52:03 -0300

    Interface e comunicacao funcionais para ambos os lados

--- client.py
+++ client.py
@@ -1,42 +1,54 @@
 import socket
 import numpy as np
 import matplotlib.pyplot as plt
-import tkinter as tk
 from encode_decode import *
-# encode, decode
 
 
 class Client:
     def __init__(self):
-        self.client_socket = socket.socket()
+        self.connection_socket = socket.socket()
         self.host = ''
         self.port = 0000
+        self.conn = ''
+        self.address = ''
 
+        self.text_message = ''
+        self.caesar = ''
         self.ascii_message = ''
         self.binary_message = ''
         self.plot_message = ''
         self.encoded_message = ''
-        self.text_message = ''
 
-        self.is_connected = False
+        self.is_host = False
 
     def connect(self):
-        self.client_socket.connect((self.host, self.port))
+        self.connection_socket.connect((self.host, self.port))
+
+    def create_connection(self):
+        self.connection_socket.bind((self.host, self.port))
+        self.connection_socket.listen(2)
+        self.conn, self.address = self.connection_socket.accept()
 
     def set_message_to_send(self, message):
         self.text_message = message
-        self.caesar = caesar(self.text_message,5,1)
-        self.ascii_message = asciiEncode(self.caeser)
+        self.caesar = caesar(self.text_message, 5, 1)
+        self.ascii_message = asciiEncode(self.caesar)
         self.binary_message = binaryEncode(self.ascii_message)
         self.plot_message = self.binary_message.copy()
         self.encoded_message = HDB3Encode(self.plot_message)
 
     def send_message(self):
         self.plot_graph(self.plot_message)
-        self.client_socket.send(self.encoded_message[0].encode())
+        if self.is_host:
+            self.conn.send(self.encoded_message[0].encode())
+        else:
+            self.connection_socket.send(self.encoded_message[0].encode())
 
     def receive_message(self):
-        hdb3_code = self.client_socket.recv(1024).decode()
+        if self.is_host:
+            hdb3_code = self.conn.recv(1024).decode()
+        else:
+            hdb3_code = self.connection_socket.recv(1024).decode()
 
         bit_array = []
         plot_data = list(''.join(hdb3_code))
@@ -51,13 +63,13 @@ class Client:
         if plt.fignum_exists(True):
             plt.close()
 
-        self.plot_graph(plot_data)
+        self.plot_graph(bit_array)
 
         self.encoded_message = hdb3_code
-        self.binary_message = HDB3Decode(self.encoded_message[0])
+        self.binary_message = HDB3Decode(self.encoded_message)
         self.ascii_message = binaryDecode(self.binary_message)
         self.caesar = asciiDecode(self.ascii_message)
-        self.text_message = caesar(self.caesar,5,0)
+        self.text_message = caesar(self.caesar, 5, 0)
 
     def plot_graph(self,message):
         if plt.fignum_exists(True):
@@ -67,71 +79,3 @@ class Client:
         index = list(np.arange(len(message)))
         plt.bar(index, message)
         plt.show()
-
-
-    # TDOD: implementar set e get do host e port e métodos com o código do koller:
-    #       - send_message
-    #       - receive_message
-    #       - plot_graph (?)
-    #       - outros que tiver
-
-# inicializa widgets da tela de conexao
-
-
-# if __name__ == '__main__':
-#     # pega as inforções do servidor para conexão
-#     host = socket.gethostname()
-#     port = input()
-#
-#     # conecta com o servidor
-#     client_socket = socket.socket()
-#     client_socket.connect((host, int(port)))
-#
-#     while True:
-#         # pega a mesagem do input e envia ao servidor
-#         message = encode(input())
-#         client_socket.send(message[0].encode())
-#
-#         # verifica se existe uma janela de gráfico aberta e fecha se existir
-#         if plt.fignum_exists(True):
-#             plt.close()
-#
-#         # plota o gráfico da mensagem codificada
-#         plt.rcParams["figure.autolayout"] = True
-#         plt.title('Enviado')
-#         index = list(np.arange(len(message[1])))
-#         plt.bar(index, message[1])
-#         plt.show()
-#
-#         # espera até receber uma mesagem do servidor
-#         data = client_socket.recv(1024).decode()
-#
-#         print('Recebido:')
-#         print(data)
-#
-#         #tranforma string recebida em um vetor para a plotagem
-#         bit_array = []
-#         plot_data  = list(''.join(data))
-#         for bit in plot_data:
-#                 if bit == '+':
-#                     bit_array.append(1)
-#                 elif bit == '-':
-#                     bit_array.append(-1)
-#                 else:
-#                     bit_array.append(0)
-#         # verifica se existe uma janela de gráfico aberta e fecha se existir
-#         if plt.fignum_exists(True):
-#             plt.close()
-#
-#         # plota o gráfico da mensagem codificada recebida
-#         plt.rcParams["figure.autolayout"] = True
-#         plt.title('Recebido')
-#         index = list(np.arange(len(bit_array)))
-#         plt.bar(index, bit_array)
-#         plt.show()
-#
-#         # decoda a mesagem recebida
-#         data = decode(data)
-#         print(data)
-#
-#     client_socket.close()
--- interface.py
+++ interface.py
@@ -8,8 +8,11 @@ class Interface:
         self.window.title('HDB3 Client')
         self.window.columnconfigure(0, weight=1)
         self.window.rowconfigure(0, weight=1)
-        self.window.geometry('400x300')
+        self.window.geometry('758x450')
+        self.window.minsize(758, 450)
+        self.window.maxsize(758, 450)
 
+        self.create_connection_toggle = tk.BooleanVar()
         self.local_connection_toggle = tk.BooleanVar()
         self.host = tk.StringVar()
         self.port = tk.StringVar()
@@ -20,44 +23,57 @@ class Interface:
         # cria frame de conexao e widgets
         self.connect_frame = tk.Frame(self.window)
         self.host_entry = tk.Entry(self.connect_frame)
+        self.local_connection_checkbutton = tk.Checkbutton(self.connect_frame)
 
         # cria frame de mensagem e widgets
         self.message_frame = tk.Frame(self.window)
         self.history_text = tk.Text(self.message_frame)
         self.message_text = tk.Text(self.message_frame)
 
+        self.history_index = 0.0
         self.init_connection_frame()
+
+        self.receive_toggle = tk.BooleanVar()
+        self.receive_toggle.set(False)
 
         self.window.mainloop()
 
     def init_connection_frame(self):
         tk.Label(self.connect_frame, text="Conectar").grid(column=0, row=0, sticky=tk.W)
 
-        tk.Checkbutton(self.connect_frame, text="Conexao local", variable=self.local_connection_toggle, command=self.check_if_local)\
+        tk.Checkbutton(self.connect_frame, text='Criar sessao', variable=self.create_connection_toggle, command=self.check_if_creating)\
             .grid(column=0, row=1, sticky=(tk.W, tk.E, tk.S, tk.N))
 
-        tk.Label(self.connect_frame, text="Host").grid(column=0, row=2, sticky=(tk.W, tk.E))
+        self.local_connection_checkbutton.config(text="Conexao local", variable=self.local_connection_toggle, command=self.check_if_local)
+        self.local_connection_checkbutton.grid(column=0, row=2, sticky=(tk.W, tk.E, tk.S, tk.N))
+
+        tk.Label(self.connect_frame, text="Host").grid(column=0, row=3, sticky=(tk.W, tk.E))
 
         self.host_entry.configure(width=7, textvariable=self.host)
-        self.host_entry.grid(column=1, row=2, sticky=(tk.W, tk.E, tk.S, tk.N))
+        self.host_entry.grid(column=1, row=3, sticky=(tk.W, tk.E, tk.S, tk.N))
 
-        tk.Label(self.connect_frame, text="Port").grid(column=0, row=3, sticky=(tk.W, tk.E, tk.S, tk.N))
+        tk.Label(self.connect_frame, text="Port").grid(column=0, row=4, sticky=(tk.W, tk.E, tk.S, tk.N))
 
-        tk.Entry(self.connect_frame, width=7, textvariable=self.port).grid(column=1, row=3, sticky=(tk.W, tk.E, tk.S, tk.N))
+        tk.Entry(self.connect_frame, width=7, textvariable=self.port).grid(column=1, row=4, sticky=(tk.W, tk.E, tk.S, tk.N))
 
-        tk.Button(self.connect_frame, command=self.connect, text='Conectar').grid(column=1, row=4, sticky=(tk.W, tk.E, tk.S, tk.N))
+        tk.Button(self.connect_frame, command=self.connect, text='Conectar').grid(column=1, row=5, sticky=(tk.W, tk.E, tk.S, tk.N))
 
         self.connect_frame.grid(column=0, row=0, sticky=(tk.W, tk.E, tk.S, tk.N))
 
     def init_message_frame(self):
-        self.history_text.config(height=20, width=100, state='disabled')
-        self.history_text.grid(column=0, row=0, columnspan=2, sticky=tk.W)
+        scrollbar = tk.Scrollbar(self.message_frame, orient='vertical', command=self.history_text.yview)
+        scrollbar.grid(column=2, row=0, sticky=(tk.W, tk.N, tk.S))
+
+        self.history_text.config(height=20, width=100, yscrollcommand=scrollbar.set)
+        self.history_text.grid(column=0, row=0, columnspan=2, sticky=(tk.W, tk.E))
 
         self.message_text.config(height=5, width=80)
-        self.message_text.grid(column=0, row=1, sticky=tk.W)
+        self.message_text.grid(column=0, row=1, rowspan=2, sticky=tk.W)
+
+        tk.Button(self.message_frame, command=self.receive_message, text='Receber').grid(column=1, row=2, sticky=(tk.W, tk.E, tk.S, tk.N))
 
         send_button = tk.Button(self.message_frame, command=self.send_message, height=2, width=20, text='Enviar')
-        send_button.grid(column=1, row=1, sticky=tk.W)
+        send_button.grid(column=1, row=1, sticky=(tk.W, tk.E, tk.N))
 
         self.message_frame.grid(column=0, row=0, sticky=(tk.W, tk.E, tk.S, tk.N), padx=5, pady=5)
 
@@ -65,12 +81,16 @@ class Interface:
         try:
             self.client.host = self.host.get()
             self.client.port = int(self.port.get())
-            self.client.connect()
+            if self.create_connection_toggle.get():
+                self.client.create_connection()
+                self.client.is_host = True
+            else:
+                self.client.connect()
             self.connect_frame.destroy()
             self.init_message_frame()
 
         except TimeoutError:
-            print("Nao foi possivel conectar a" + self.client.host + ":" + self.client.port)
+            print("Nao foi possivel conectar a" + self.client.host + ":" + str(self.client.port))
 
     def check_if_local(self):
         if self.local_connection_toggle.get():
@@ -80,12 +100,90 @@ class Interface:
             self.host_entry.configure(state='normal')
             self.host.set('')
 
+    def check_if_receiving(self):
+        if self.receive_toggle.get():
+            self.receive_message()
+
+    def check_if_creating(self):
+        if self.create_connection_toggle.get():
+            self.local_connection_checkbutton.config(state='disabled')
+            self.host_entry.configure(state='disabled')
+            self.host.set(socket.gethostname())
+        else:
+            self.local_connection_checkbutton.config(state='normal')
+            self.host_entry.configure(state='normal')
+            self.host.set('')
+
     def send_message(self):
         message = self.message_text.get('0.0', tk.END)
+
+        message = message.removesuffix('\n')
+
         self.client.set_message_to_send(message)
 
+        # pega as variaveis do client e armazena no tk.Text de historico
+        self.history_text.insert(tk.END, 'ENVIADO:\n')
+
+        self.history_text.insert(tk.END, '- Texto:\n')
+
+        self.history_text.insert(tk.END, self.client.text_message)
+        self.history_text.insert(tk.END, '\n\n')
+
+        self.history_text.insert(tk.END, '- Encriptado:\n')
+
+        self.history_text.insert(tk.END, self.client.caesar)
+        self.history_text.insert(tk.END, '\n\n')
+
+        self.history_text.insert(tk.END, '- ASCII:\n')
+
+        self.history_text.insert(tk.END, self.client.ascii_message)
+        self.history_text.insert(tk.END, '\n\n')
+
+        self.history_text.insert(tk.END, '- Binario:\n')
+
+        self.history_text.insert(tk.END, self.client.binary_message)
+        self.history_text.insert(tk.END, '\n\n')
+
+        self.history_text.insert(tk.END, '- Codificado:\n')
+
+        self.history_text.insert(tk.END, self.client.encoded_message[0])
+        self.history_text.insert(tk.END, '\n\n\n\n')
+
+        self.window.update()
         self.client.send_message()
-        self.receive_message()
+        self.receive_toggle.set(True)
+
+        self.message_text.delete('0.0', tk.END)
 
     def receive_message(self):
         self.client.receive_message()
+
+        # pega as variaveis do client e armazena no tk.Text de historico
+        self.history_text.insert(tk.END, 'RECEBIDO:\n')
+
+        self.history_text.insert(tk.END, '- Codificado:\n')
+
+        self.history_text.insert(tk.END, self.client.encoded_message[0])
+        self.history_text.insert(tk.END, '\n\n')
+
+        self.history_text.insert(tk.END, '- Binario:\n')
+
+        self.history_text.insert(tk.END, self.client.binary_message)
+        self.history_text.insert(tk.END, '\n\n')
+
+        self.history_text.insert(tk.END, '- ASCII:\n')
+
+        self.history_text.insert(tk.END, self.client.ascii_message)
+        self.history_text.insert(tk.END, '\n\n')
+
+        self.history_text.insert(tk.END, '- Encriptado:\n')
+
+        self.history_text.insert(tk.END, self.client.caesar)
+        self.history_text.insert(tk.END, '\n\n')
+
+        self.history_text.insert(tk.END, '- Texto:\n')
+
+        self.history_text.insert(tk.END, self.client.text_message)
+        self.history_text.insert(tk.END, '\n\n\n\n')
+
+        self.receive_toggle.set(False)
